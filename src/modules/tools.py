1524f09cff2bf2fcb164f00e0096ac4a3e146c33
Toxgen <Hydro_Toxgen@outlook.com>
2023-09-05 21:25:22 -0400

    refer back to the phind thing (v.0.011)

--- src/modules/tools.py
+++ src/modules/tools.py
@@ -1,3 +1,23 @@
+global __mob_data, __mob_drops, __possible_mobs
+
+__mob_data = [ 
+    # woods [0][0 - 2]
+    ["goblin", 8, 2, 3, 1, None],
+    ["slime", 12, 3, 4, 3, None],
+    ["wolf", 7, 5, 7, 2, None]
+    # plains [1][0 - ?]
+]
+
+__mob_drops = [
+    # goblin
+    ["_hide", "_leg", "_sword", "_staff", "_chestplate", "_legging", "_helmet"]
+
+]
+
+__possible_mobs = [
+    "goblin", "slime", "wolf"
+]
+
 def drops(mob: str) -> list:
     """
     drops(mob)
@@ -59,7 +79,7 @@ def drops(mob: str) -> list:
         case _:
             raise Exception("1st: Oh NAHHHHHHHHHHHHHHHHH")
 
-def counting_drop(list: list, mob: str):
+def counting_drop(check: list[str], mob: str):
     """
     counting_drop(list, mob)
     list: cct inventory
@@ -72,26 +92,26 @@ def counting_drop(list: list, mob: str):
             # make a list of the drops
             # maybe make the mob drops global so every single function can use it
             # or even make a mob a global variable
-            g_hide = list.count("goblin_hide")
+            g_hide = check.count("goblin_hide")
 
-            g_leg = list.count("goblin_leg")
+            g_leg = check.count("goblin_leg")
             
-            g_sword = list.count("goblin_sword")
+            g_sword = check.count("goblin_sword")
 
-            g_staff = list.count("goblin_staff")
+            g_staff = check.count("goblin_staff")
 
-            g_chest = list.count("goblin_chestplate")
+            g_chest = check.count("goblin_chestplate")
 
-            g_legging = list.count("goblin_legging")
+            g_legging = check.count("goblin_legging")
 
-            g_helmet = list.count("goblin_helmet")
+            g_helmet = check.count("goblin_helmet")
 
             return [g_hide, g_leg, g_sword, g_staff, g_chest, g_legging, g_helmet] # dont forget to index this into the game.py thing
     
         case _:
             raise Exception("Error: This shouldn't happen, p.s. check mob arg")
               
-def printingDrops(preinv: list, mob: str):
+def printingDrops(preinv: list[str]):
     """
     print em drops
     (the counted drop list, self.mob)
@@ -157,13 +177,7 @@ def printingInv(inv: dict) -> None:
 def returnMob(hp: int, location: str) -> list:
     import random as r
     from random import randint
-    md = [ 
-        # woods [0][0 - 2]
-        ["goblin", 8, 2, 3, 1, None],
-        ["slime", 12, 3, 4, 3, None],
-        ["wolf", 7, 5, 7, 2, None]
-        # plains [1][0 - ?]
-    ]
+
     if hp > 50:
         _hp_multi = round(hp/20 * 0.5) 
         _def_multi = round(hp/20 * 0.5)
@@ -174,23 +188,22 @@ def returnMob(hp: int, location: str) -> list:
         _def_multi = 1
         _attk_mul = 1
 
-    def __wood_mobs(md: list, mob: str, hp: int, defe: int, attk: int) -> list: 
+    def __wood_mobs(__mob_data: list, mob: str, hp: int, defe: int, attk: int) -> list: 
         # there should be a easier way to do this way less boilerplate code
         match mob:
             case "goblin":
-                for i in range(5):
-                    return [md[0][0], 
-                            md[0][1] + hp + hp * 0.5, 
-                            md[0][2] + attk + attk * 0.5,
-                            md[0][3] + attk + attk * 0.5,
-                            md[0][4] + defe + defe * 0.5]
+                    return [__mob_data[0][0], 
+                            __mob_data[0][1] + hp + hp * 0.5, 
+                            __mob_data[0][2] + attk + attk * 0.5,
+                            __mob_data[0][3] + attk + attk * 0.5,
+                            __mob_data[0][4] + defe + defe * 0.5]
         
             case "slime":
-                    return [md[1][0], 
-                            md[1][1] + hp + hp * 0.5, 
-                            md[1][2] + attk + attk * 0.5,
-                            md[1][3] + attk + attk * 0.5,
-                            md[1][4] + defe + defe * 0.5]
+                    return [__mob_data[1][0], 
+                            __mob_data[1][1] + hp + hp * 0.5, 
+                            __mob_data[1][2] + attk + attk * 0.5,
+                            __mob_data[1][3] + attk + attk * 0.5,
+                            __mob_data[1][4] + defe + defe * 0.5]
             case "wolf":
                 pass # work on this later
 
@@ -201,38 +214,35 @@ def returnMob(hp: int, location: str) -> list:
         case "woods":
             x = r.randint(0, 20)
             if x > 12:
-                return __wood_mobs(md, md[0][0], _hp_multi, _def_multi, _attk_mul)
+                return __wood_mobs(__mob_data, __mob_data[0][0], _hp_multi, _def_multi, _attk_mul)
             
             if x > 8:
-                return __wood_mobs(md, md[1][0], _hp_multi, _def_multi, _attk_mul)
+                return __wood_mobs(__mob_data, __mob_data[1][0], _hp_multi, _def_multi, _attk_mul)
             
             if x > 4:
-                return __wood_mobs(md, md[2][0], _hp_multi, _def_multi, _attk_mul)
+                return __wood_mobs(__mob_data, __mob_data[2][0], _hp_multi, _def_multi, _attk_mul)
         case _:
             raise Exception("ERROR 1: MissType")
         
-def insertingMobDrops(preinv: list, inv: list, mob: str) -> list:
-    __mob_drops = [
-        # goblin
-        ["_hide", "_leg", "_sword", "_staff", "_chestplate", "_legging", "_helmet"]
+def insertingMobDrops(preinv: list[str], inv: list, mob: str) -> list:
 
-    ]
-
-    __possible_mobs = [
-        ["goblin", "slime", "wolf"]
-    ]
-
-    __index  = __possible_mobs.index(mob)
+    __index  = __possible_mobs.index(mob) 
 
     for i, thing in enumerate(__mob_drops[__index]):
 
-        if f"{mob}{thing}" not in inv: 
-            inv.update({f"{mob}{thing}": 0})
+        if f"{mob}{thing}" not in inv and f"{mob}{thing}" in preinv: 
+            inv.append(["goblin_hide", 1])
+            continue
 
-        inv[f"{mob}{thing}"] += preinv[i]
-    
+        __index = inv.index([f"{mob}{thing}", int])
+
     return inv
 
 
 if __name__ == '__main__':
-    pass
+    ex = [["a", 1], ["b", 2]]
+    index = ex.index(["a", ])
+    print(index)
+    # inv = []
+    # inv = insertingMobDrops(["goblin_hide", "goblin_sword", "goblin_chestplate", "goblin_hide"], inv=inv, mob="goblin")
+    # print(inv)
